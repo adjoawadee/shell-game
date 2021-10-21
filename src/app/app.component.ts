6046f3149f4af5c59999e0ca4b6ea853753f12ac
adjoa <adjoa.wadee@gmail.com>
2021-10-21 18:55:12 +0200

    Angular Material CDK, better movement of shell

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,5 @@
+import { CdkDrag, CdkDragDrop, CdkDragStart, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { Component, OnInit, ElementRef, ViewChild, Renderer2, AfterViewInit } from '@angular/core';
-
-interface MousePosition {
-  x: number;
-  y: number;
-}; 
 
 @Component({
   selector: 'app-root',
@@ -11,44 +7,64 @@ interface MousePosition {
   styleUrls: ['./app.component.scss']
 })
 
-export class AppComponent implements OnInit, AfterViewInit{
-  @ViewChild('shellOne') shellOne: ElementRef;
-
+export class AppComponent implements OnInit{
+  @ViewChild('ballElement') ballElement: ElementRef
   public title = 'shell-game';
-  private dragOffset: number[] = [0, 0]; 
-  private mouseDown: boolean = false;
+  private currentShell: HTMLElement;
+  public listOfShells = [
+    { value:'shell-one', disabled: false },
+    { value:'shell-two', disabled: false },
+    { value:'shell-three', disabled: false }
+  ]
+  
+  public dropAreaData = [
+    { value: 'ball',
+      disabled: true
+    }
+  ]
 
-  constructor(private renderer: Renderer2, private el: ElementRef) {}
+  public dragPosition = {
+    x: 0,
+    y: 0
+  };
+
+  constructor(private renderer: Renderer2, 
+              private el: ElementRef
+            ) {}
 
   ngOnInit(): void {
   }
 
-  ngAfterViewInit(): void {
-    console.log(this.shellOne);
+  getCoordinates(event: CdkDragStart) {
+    this.currentShell = event.source.element.nativeElement;
   }
 
-  getCoordinates(event: any) {
-    this.dragOffset = [
-      this.shellOne.nativeElement.offsetLeft - event.clientX,
-      this.shellOne.nativeElement.offsetTop - event.clientY
-    ];
-  }
-
-  endMove() {
-    console.log('move ended');
-  }
 
   dragShell(event: any) {
-    event.preventDefault();
-    let mousePos: MousePosition = {
-      x : event.clientX,
-      y : event.clientY
-    }
-    let left = `${mousePos.x + this.dragOffset[0]}px`;
-    let top = `${mousePos.y + this.dragOffset[1]}px`;
+    // get the ball with shell
+      // if the position of the box is the same as the ball
+      // group the ball and the box
+    // disable shells
+    // shuffel shells
+    // enable click
 
-    this.shellOne.nativeElement.style.left = left;
-    this.shellOne.nativeElement.style.top = top;
+    let shellPosition = {
+      x: this.currentShell.getBoundingClientRect().left,
+      y: this.currentShell.getBoundingClientRect().top
+    };
+    let ballPosition = {
+      x: this.ballElement.nativeElement.getBoundingClientRect().left,
+      y: this.ballElement.nativeElement.getBoundingClientRect().top
+    };
+
+    if (ballPosition.x - shellPosition.x >= 1 && 
+        ballPosition.x - shellPosition.x <= 40 ) {
+          this.listOfShells.forEach(shell => {
+          shell.disabled=true;
+          });
+    } else {
+      console.log('busted');
+    }
   }
 
 }
